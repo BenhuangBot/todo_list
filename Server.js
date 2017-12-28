0ecfc7de055143811b3440039c3e1fda456d324e
BenhuangBot <king810406@gmail.com>
2017-12-28 12:24:53 +0800

    'Create_Post_method'

--- Server.js
+++ Server.js
@@ -1,1 +1,37 @@
-var http = require("http");
+var Http = require("http");
+var Router = require("router");
+var BodyParser = require("body-parser");
+var server;
+var router;
+var counter = 0;
+var todoList = {};
+
+
+router = new Router();
+
+server = Http.createServer(function (request, response) {
+    router(request,response,function(error){
+        if(!error){
+            response.writeHead(404);
+        }else{
+            console.log(error.message, error.stack);
+            response.writeHead(400);
+        }
+        response.end('RESTful API Server is running!');
+    });
+});
+router.use(BodyParser.text());
+function createItem(request,response) {
+    var id = counter++;
+    var item = request.body;
+    console.log('Create item', id, item);
+    todoList[id] = item;
+    response.writeHead(201,{'Content-Type' : 'text/plain', 'Location' : '/todo/' + id});
+    response.end(item);
+}
+router.post('/todo', createItem);
+
+
+server.listen(3000,function (){
+    console.log('Listening on port 3000');
+});
